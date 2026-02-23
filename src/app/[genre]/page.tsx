2e6670136f70e702df44cae489c992bdbfec7fcb
Məftun Məmmədov <mftnmusic6386@gmail.com>
2026-02-23 16:57:59 +0400

    seo-added-for-movie-and-genre-routes

--- src/app/[genre]/page.tsx
+++ src/app/[genre]/page.tsx
@@ -1,10 +1,50 @@
+import { getDataList } from "@/api/helpers";
 import { getGenreDetails } from "@/api/helpers/genre";
 import MovieCard from "@/components/MovieCard";
+import { Genre } from "@/types";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
+import { cache } from "react";
+
+export const revalidate = 3600;
+
+export const getGenre = cache(async (slug: string) => {
+  return await getGenreDetails(slug);
+});
+
+export const generateStaticParams = async () => {
+  const genres = await getDataList<Genre>("mov_genres", { select: "slug" });
+  return genres.map((genre) => ({ genre: genre.slug }));
+};
+
+export const generateMetadata = async ({
+  params,
+}: {
+  params: Promise<{ genre: string }>;
+}): Promise<Metadata | undefined> => {
+  const { genre: slug } = await params;
+  const genreData = await getGenre(slug);
+
+  if (!genreData) return;
+  const { genre } = genreData;
+
+  const title_description = {
+    title: genre.title,
+    description: `Watch ${genre.title} movies`,
+  };
+
+  return {
+    ...title_description,
+    robots: {
+      index: true,
+      follow: true,
+    },
+  };
+};
 
 const MoviesByGenre = async ({ params }: { params: Promise<{ genre: string }> }) => {
   const { genre: slug } = await params;
-  const genreData = await getGenreDetails(slug);
+  const genreData = await getGenre(slug);
 
   if (!genreData) {
     notFound();
